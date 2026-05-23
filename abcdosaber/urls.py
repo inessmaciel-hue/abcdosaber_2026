a66524a680f9aeea76a43c3f890765bd578ad0a0
inessmaciel-hue <inessmaciel@gmail.com>
2026-05-22 21:44:52 -0300

    Aula 05 criacao de views, templates e definicao de url para app titulo

--- abcdosaber/urls.py
+++ abcdosaber/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from titulo import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_view),
-    path('cba', views.show_template),
+    path('titulo/', include ("titulo.urls")),
+    
+    
+    # path('cba', views.show_template),
 ]
--- titulo/views.py
+++ titulo/views.py
@@ -2,8 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def show_view(request):
+def show_views(request):
     return HttpResponse('<p>Minha View do App Titulos</p>')
 
 def show_template(request):
     return HttpResponse('<p>Esse e o template do site</>')
+
+def listar_exemplo(request):
+    pagina = 'ola'
+    return HttpResponse(pagina)
+
+def abc(request):
+    pagina = 'ABC!'
+    return HttpResponse(pagina)
+
+def index(request):
+    return render(request, 'index.html')
+
+def teste(request):
+    return render(request, 'teste.html')
