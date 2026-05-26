6f342fd9601aa6e43b821600d8417a495e093401
inessmaciel-hue <inessmaciel@gmail.com>
2026-05-25 21:46:36 -0300

    correcao views.py do App Titulos

--- titulo/views.py
+++ titulo/views.py
@@ -22,17 +22,17 @@ def index(request):
 # def teste(request):
     # return render(request, 'teste.html')
 
-def aluno(request):
-    return render(request, 'aluno.html')
+# def aluno(request):
+#     return render(request, 'aluno.html')
 
-def instrutor(request):
-    return render(request, 'instrutor.html')
+# def instrutor(request):
+#     return render(request, 'instrutor.html')
 
-def tipo_de_atividade(request):
-    return render(request,'tipo_de_atividade.html')
+# def tipo_de_atividade(request):
+#     return render(request,'tipo_de_atividade.html')
 
-def turma(request):
-    return render(request,'turma.html')
+# def turma(request):
+#     return render(request,'turma.html')
 
-def utilitarios(request):
-    return render(request, 'utilitarios.html')
+# def utilitarios(request):
+#     return render(request, 'utilitarios.html')
